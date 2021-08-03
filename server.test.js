ccb56b161d392dce174ec4e61e9841d03232f5b0
Gilang <gilangvirga271@gmail.com>
2021-08-03 15:44:20 +0700

    update endpoint

--- server.test.js
+++ server.test.js
@@ -21,8 +21,8 @@ it('Call the /pong endpoint', async done => {
     expect(res.text).toBe('Pong!')
     done()
 })
-it('Call the /hello/:name endpoint', async done => {
-    const res = await request.get('/hello/Gilang')
+it('Call the /hellos/:name endpoint', async done => {
+    const res = await request.get('/hellos/Gilang')
     expect(res.status).toBe(200)
     expect(res.body.message).toBe('Hello Gilang')
     done()
